8b7db39b846c8e76594571667a21ef92e23dc311
Yiwen Zhang <yiwenmary@gmail.com>
2024-02-16 21:46:47 -0500

    make comments for each step

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function getLearnerData(course, ag, submissions) {
   const results = [];
 
   // Parse submission data.
-//   console.log(`Submission Data:`, submissions );
+  //   console.log(`Submission Data:`, submissions );
   // Check to see if the submission was late; if so, deduct 10% of the maximum possible points.
   // Find existing data for this learner, if any.
   // If the learner already has data, add the new score to the existing data.
@@ -88,70 +88,87 @@ function getLearnerData(course, ag, submissions) {
 
   //==== PUT CODE HERE =====//
 
-  //   create a list of learners with a list of submission information
+  // 1. create a list of learners with a list of submission information
   const learners = {};
   submissions.forEach((submit) => {
     if (!(submit.learner_id in learners)) {
-        learners[submit.learner_id] = [[submit.assignment_id, submit.submission.submitted_at, submit.submission.score]]
+      learners[submit.learner_id] = [
+        [
+          submit.assignment_id,
+          submit.submission.submitted_at,
+          submit.submission.score,
+        ],
+      ];
     } else {
-        learners[submit.learner_id].push([submit.assignment_id, submit.submission.submitted_at, submit.submission.score])
+      learners[submit.learner_id].push([
+        submit.assignment_id,
+        submit.submission.submitted_at,
+        submit.submission.score,
+      ]);
     }
-  })
+  });
 
   console.log(learners);
 
-//   create a list of learners' id
+  // 2. create a list of learners' id
   const learners_id = Object.keys(learners);
   console.log(learners_id);
 
-//   create the result list of objects/learners
-  const learners_avg =[];
+  // 3. create the result list of objects/learners
+  const learners_avg = [];
+  // for each learner
   learners_id.forEach((id) => {
-    console.log(learners[id])
+    console.log(learners[id]);
     const all_submits = learners[id];
 
     // get the average for each learner
     let sum_score = 0;
     let sum_total = 0;
     let avg;
-    let learner_info = { 'id': id};
-    for (const submit of all_submits) {  // 
-        sum_score += submit[2];
-        const assignment =  ag.assignments.find(a => a.id === submit[0])
-        // console.log(assignment)
-        sum_total += assignment.points_possible;
-        avg = sum_score / sum_total;
-        avg = avg.toFixed(2);  // to have exactly two decimal digits 
-        learner_info[submit[0]] = (submit[2] / assignment.points_possible).toFixed(2);
+    // for final output and the format: the ID of the learner for which this data has been collected
+    let learner_info = { id: id };
+    for (const submit of all_submits) {
+      sum_score += submit[2];
+      const assignment = ag.assignments.find((a) => a.id === submit[0]);
+      // console.log(assignment)
+      sum_total += assignment.points_possible;
+      avg = sum_score / sum_total;
+      avg = avg.toFixed(2); // to have exactly two decimal digits
+      // for final output and the format: the learner's weighted average score
+      learner_info["avg"] = avg;
+
+      // for final output and the format: each assignment: submission.score / points_possible
+      learner_info[submit[0]] = (
+        submit[2] / assignment.points_possible
+      ).toFixed(2);
     }
-    learner_info['avg'] = avg;
+    
     console.log(sum_score, sum_total, avg);
     console.log(learner_info);
-  })
+  });
 
+    // console.log(learners_avg);
 
-//   console.log(learners_avg);
-  
   return results;
 }
 
+console.log(getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions))
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(result);
 
-
 // --- FYI ----------------
-    // const example_result = [
-    //     {
-    //       id: 125,
-    //       avg: 0.985, // (47 + 150) / (50 + 150)
-    //       1: 0.94, // 47 / 50
-    //       2: 1.0, // 150 / 150
-    //     },
-    //     {
-    //       id: 132,
-    //       avg: 0.82, // (39 + 125) / (50 + 150)
-    //       1: 0.78, // 39 / 50
-    //       2: 0.833, // late: (140 - 15) / 150
-    //     },
-    //   ];
+// const example_result = [
+//     {
+//       id: 125,
+//       avg: 0.985, // (47 + 150) / (50 + 150)
+//       1: 0.94, // 47 / 50
+//       2: 1.0, // 150 / 150
+//     },
+//     {
+//       id: 132,
+//       avg: 0.82, // (39 + 125) / (50 + 150)
+//       1: 0.78, // 39 / 50
+//       2: 0.833, // late: (140 - 15) / 150
+//     },
+//   ];
